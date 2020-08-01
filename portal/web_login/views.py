fb24e1f8f1a8e165b81073ef33d1e8556033cbf2
Jonathan Guzmán <jonguz5@hotmail.com>
2020-07-31 23:09:40 -0600

    corregida ruta incorrecta de plantilla

--- portal/web_login/views.py
+++ portal/web_login/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
 class InicioView(TemplateView):
-    template_name = "web login/inicio.html"
+    template_name = "web_login/inicio.html"
