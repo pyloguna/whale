439748a03176a6cd7d2d8297b85e6db7665c07df
Jonathan Guzmán <jonguz5@hotmail.com>
2020-08-01 19:55:06 -0600

    actualizadas urls

--- portal/web_login/urls.py
+++ portal/web_login/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = "login_web"
 urlpatterns = [
-    path('', views.InicioView.as_view(), name="inicio")
+    path('', views.InicioView.as_view(), name="inicio"),
+    path('login', views.BasicLoginView.as_view(), name='login'),
+    path('registro', views.RegistroView.as_view(), name='registro'),
+    path('logout', views.LogoutView.as_view(), name='logout')
 ]
