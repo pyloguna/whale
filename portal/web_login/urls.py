e7d0e10279248a72d28cbd7df8c29bcd9f89f41e
Jonathan Guzmán <jonguz5@hotmail.com>
2020-08-01 22:00:03 -0600

    agregada url faltante

--- portal/web_login/urls.py
+++ portal/web_login/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.InicioView.as_view(), name="inicio"),
     path('login', views.BasicLoginView.as_view(), name='login'),
     path('registro', views.RegistroView.as_view(), name='registro'),
-    path('logout', views.LogoutView.as_view(), name='logout')
+    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('dashboard', views.DashBoardView.as_view(), name='dashboard')
 ]
