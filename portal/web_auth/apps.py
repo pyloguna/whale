5bfa4a4c83501b775f8ed3bb2e09b3f8a2f771f6
Jonathan Guzman <jonguz5@hotmail.com>
2020-09-29 23:02:47 -0600

    Asignado codigo clave del proyecto

--- portal/web_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AuthConfig(AppConfig):
-    name = 'web_auth'
--- portal/web_auth/static/web_auth/js/otp_upd.js
+++
@@ -1,8 +0,0 @@
-var hotp_img;
-window.onload = function(){
-    hotp_img = document.getElementById("hotp_img");
-    var t = new Date();
-    setInterval(()=>{
-        hotp_img.src = "/qr?nocache=" + t.getTime();
-    },5000);
-};
